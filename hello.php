233d9cb745bed8dc4912e78599b2173345a1b559
saravanan <saravinbox@gmail.com>
2018-08-31 14:04:38 +0530

    hello.php added
    hello.php added

---
+++ hello.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post"><script>alert('hacked')</script>
+	<?php  echo "hello i am in the first page sample"; ?>
+	<input type="submit" value="submit" >
+</form>
+</body>
+</html>
