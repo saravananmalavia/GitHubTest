8b3376d297283197a607c41999f035684ae756f0
saravanan <saravinbox@gmail.com>
2018-08-31 15:12:07 +0530

    Revert "hello.php added"
    This reverts commit 233d9cb745bed8dc4912e78599b2173345a1b559.

--- hello.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-<form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post"><script>alert('hacked')</script>
-	<?php  echo "hello i am in the first page sample"; ?>
-	<input type="submit" value="submit" >
-</form>
-</body>
-</html>
